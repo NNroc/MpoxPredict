5c6eb374f151b1be4cbeea32836fe7faa98968a2
NNroc <1581333251@qq.com>
2023-02-22 19:06:47 +0800

    modify function

--- data/data_city_size.py
+++ data/data_city_size.py
@@ -2,14 +2,10 @@ import pandas as pd
 import datetime as dt
 import csv
 import os
-import pandas as pd
-import datetime as dt
 import xlrd
 
-from mpox.entity.entity import Records, Country
-
 data_flight_filename = '20230221 数据整理(军事医学研究院).xlsx'
-data_flight_save_filename = 'data_flight.csv'
+# data_flight_save_filename = 'data_flight.csv'
 
 # 出现的所有城市名字
 all_city_name = []
@@ -19,25 +15,18 @@ all_date_series = []
 data_flight = dict()
 
 # 获取 xlsx
-df = pd.read_excel(data_flight_filename)
-data = df.values
+df1 = pd.read_excel(data_flight_filename, sheet_name='Sheet1')
+data = df1.values
+# df2 = pd.read_excel(data_flight_filename, sheet_name='Sheet3')
 for row in data:
     if row[0] not in all_date_series:
         all_date_series.append(row[0])
-        data_flight[row[0]] = [row[2], row[3]]
-    else:
-        data_flight[row[0]] = [data_flight[row[0]][0] + row[2], data_flight[row[0]][1] + row[3]]
     if row[1] not in all_city_name:
         all_city_name.append(row[1])
+    data_flight[row[0]] = row[2] + row[3]
 
 # 根据城市名排序
 all_city_name.sort()
-
-# 保存航班文件
-with open(data_flight_save_filename, 'w', encoding='utf-8') as f:
-    f.writelines('date,到达国家,国内航司,境外航司''\n')
-    for row in data_flight:
-        f.writelines(row + ',中国,' + str(data_flight[row][0]) + ',' + str(data_flight[row][1]) + '\n')
 
 # 筛选城市规模信息
 data_city_size_filename = '城市规模参数.xlsx'
@@ -48,7 +37,8 @@ data_city_size = dict()
 # 未筛查到的城市
 city_not_have = list.copy(all_city_name)
 
-df = pd.read_excel(data_city_size_filename)
+# 读取sheet列表的元素
+df = pd.read_excel(data_city_size_filename, sheet_name='all')
 data = df.values
 for row in data:
     if row[1][-1] == '市':
@@ -58,8 +48,15 @@ for row in data:
         data_city_size[row[1]] = [row[19], row[31]]
         city_not_have.remove(row[1])
 
+# 保存航班文件,xlsx操作更方便......
+# with open(data_flight_save_filename, 'w', encoding='gbk') as f:
+#     other_city=''
+#     f.writelines('date,到达国家,国内外航司'+other_city+'\n')
+#     for row in data_flight:
+#         f.writelines(row + ',中国,' + str(data_flight[row][0]) + ',' + str(data_flight[row][1]) + '\n')
+
 # 保存筛选过的城市规模参数文件
-with open(data_city_size_save_filename, 'w', encoding='utf-8') as f:
+with open(data_city_size_save_filename, 'w', encoding='gbk') as f:
     f.writelines('city,Proportion of high school and above,age60''\n')
     for row in data_city_size:
         f.writelines(row + ',' + str(data_city_size[row][0]) + ',' + str(data_city_size[row][1]) + '\n')
